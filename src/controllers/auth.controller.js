601dd63f32ee50ff0d6f76d64396625f11d0ddc6
Sawant-Saish <work.saishsawant@gmail.com>
2026-05-23 10:10:11 +0530

    login api/feature

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -60,6 +60,65 @@ async function userRegisterController(req, res) {
   }
 }
 
+/**
+ *
+ * @description Login user
+ * @route POST /api/auth/login
+ */
+async function userLoginController(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    // Check if user exists
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    // Check password
+    const isMatch = await user.comparePassword(password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "3d",
+    });
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+}
+
 module.exports = {
   userRegisterController,
+  userLoginController,
 };
